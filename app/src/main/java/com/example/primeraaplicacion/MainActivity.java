1d7175ae1caf5c601e8bc1c422a69ad84ef69a94
Said Torres <sesaid17@hotmail.com>
2020-07-30 19:58:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.primeraaplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.correo);
+        et2 = (EditText)findViewById(R.id.contrasena);
+        tv1 = (TextView)findViewById(R.id.estatus);
+    }
+
+    public void estatus(View view){
+        String Correo = et1.getText().toString();
+        String Contrasena = et2.getText().toString();
+
+        //String Correo = "test@test.com";
+        //String Contrasena = "1234";
+
+        String CorreoCorrecto = "test@test.com";
+        String ContrasenaCorrecta = "1234";
+
+        //Toast toast1 = Toast.makeText(getApplicationContext(),"Las credenciales son incorrectas.", Toast.LENGTH_SHORT);
+        Toast toast1 = Toast.makeText(getApplicationContext(),"C: "+Correo+" P: "+Contrasena+" Tipo: "+Contrasena.getClass().getName(), Toast.LENGTH_SHORT);
+
+        if (Correo == CorreoCorrecto && Contrasena == ContrasenaCorrecta){
+            tv1.setText("Estatus: Has iniciado sesión correctamente.");
+            toast1.show();
+        } else {
+            tv1.setText("Estatus: No has iniciado sesión aún.");
+            toast1.show();
+        }
+    }
+
+}
